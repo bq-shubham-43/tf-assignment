8c6cf916fb119ed8ceec843b32ddd293a59068f4
bq-shubham-43 <shubham.mishra@betaque.com>
2024-10-16 18:14:27 +0530

    configured providers

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = "versatile-nomad-438205-q7"
+region     = "us-central1"
+zone       = "us-central1-a"
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The ID of the project."
+  type        = string
+}
+
+variable "region" {
+  description = "The region for the resources."
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone for the resources."
+  type        = string
+}
