cfc7c31745d3df96839f2cb84dcb754d1b2b9974
bq-shubham-43 <shubham.mishra@betaque.com>
2024-10-22 11:03:06 +0530

    update root main and varaible files

--- main.tf
+++ main.tf
@@ -1,27 +1,27 @@
 module "vpc1" {
   source          = "./modules/vpc"
-  vpc_name        = "vpc-network-1"
-  subnet_name     = "subnet-1"
-  subnet_ip_range = "10.0.1.0/24"
-  reserved_subnet_ip_range = "192.168.1.0/24" # Reserved subnet range for internal load balancer
-  allow_ports     = ["80", "443"]
-  region          = "us-central1"
+  vpc_name        = var.vpc1_name
+  subnet_name     =var.vpc1_subnet_name
+  subnet_ip_range = var.vpc1_subnet_ip_range
+  reserved_subnet_ip_range = var.vpc1_reserved_subnet_ip_range # Reserved subnet range for internal load balancer
+  allow_ports     = var.allow_ports
+  region          =  var.region
 }
 
 module "vpc2" {
-  source          = "./modules/vpc"
-  vpc_name        = "vpc-network-2"
-  subnet_name     = "subnet-2"
-  subnet_ip_range = "10.0.2.0/24"
-  reserved_subnet_ip_range = "192.168.2.0/24" # Reserved subnet range for internal load balancer
-  allow_ports     = ["80", "443"]
-  region          = "us-central1"
+  source                   = "./modules/vpc"
+  vpc_name                 = var.vpc2_name
+  subnet_name              = var.vpc2_subnet_name
+  subnet_ip_range          = var.vpc2_subnet_ip_range
+  reserved_subnet_ip_range = var.vpc2_reserved_subnet_ip_range
+  allow_ports              = var.allow_ports
+  region                   = var.region
 }
 
 # MIG for VPC-1
 module "mig_vpc1" {
   source              = "./modules/mig"
-  instance_group_name = "mig-instance-group-1"
+  instance_group_name = var.mig_vpc1_name
   vpc_name            = module.vpc1.vpc_name
   region              = var.region
   zone                = var.zone
@@ -31,7 +31,7 @@ module "mig_vpc1" {
 # MIG for VPC-2
 module "mig_vpc2" {
   source              = "./modules/mig"
-  instance_group_name = "mig-instance-group-2"
+  instance_group_name = var.mig_vpc2_name
   vpc_name            = module.vpc2.vpc_name
   region              = var.region
   zone                = var.zone
@@ -45,6 +45,27 @@ module "http_lb" {
   region         = var.region
   mig1_self_link = module.mig_vpc1.instance_group_self_link  # First MIG
   mig2_self_link = module.mig_vpc2.instance_group_self_link  # Second MIG
+}
+# Internal Load Balancer for VPC 1
+module "internal_lb_vpc1" {
+  source              = "./modules/internal_lb"
+  vpc_name            = module.vpc1.vpc_name
+  vpc_id              = module.vpc1.vpc_self_link
+  subnet_id           = module.vpc1.subnet_id
+  reserved_subnet_ip_range = var.vpc1_reserved_subnet_ip_range
+  region              = var.region
+  instance_group_name = module.mig_vpc1.instance_group_self_link
+}
+
+# Internal Load Balancer for VPC 2
+module "internal_lb_vpc2" {
+  source              = "./modules/internal_lb"
+  vpc_name            = module.vpc2.vpc_name
+  vpc_id              = module.vpc2.vpc_self_link
+  subnet_id           = module.vpc2.subnet_id
+  reserved_subnet_ip_range = var.vpc2_reserved_subnet_ip_range
+  region              = var.region
+  instance_group_name = module.mig_vpc2.instance_group_self_link
 }
 
 # VPC Peering 
